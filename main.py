d5efd8adf09b4c150a71d8871ea1d566c43b3d0f
LuningGuo <guoluning_1222@outlook.com>
2025-08-18 08:49:24 +0800

    commit

--- main.py
+++ main.py
@@ -15,13 +15,11 @@ from pydoll.protocol.network.types import CookieParam
 
 
 class BeikeMapSpider:
-    def __init__(self, headless: bool = False) -> None:
-        self.headless = headless
+    def __init__(self) -> None:
         self.cookies = None
         self.progress = None
         self.db_conn = None
-        self.tab = None
-        self.ds = datetime.today().strftime(r'%Y%m%d')
+        self.ds = '20250815'  # datetime.today().strftime(r'%Y%m%d')
         self.MIN_LATITUTE, self.MAX_LATITUTE = 30.66, 31.89
         self.MIN_LONGITUDE, self.MAX_LONGITUDE = 120.86, 122.20
 
@@ -41,6 +39,7 @@ class BeikeMapSpider:
             'district_finished': False,
             'bizcircle_latitute': self.MIN_LATITUTE,
             'community_latitute': self.MIN_LATITUTE,
+            'house_community': -1
         }
         progress_path = pathlib.Path('config/progress.json')
         if progress_path.exists():
@@ -55,8 +54,9 @@ class BeikeMapSpider:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         # save cookies
-        with open('config/cookies.json', mode='w') as f:
-            json.dump(self.cookies, f, indent=2)
+        if self.cookies is not None:
+            with open('config/cookies.json', mode='w') as f:
+                json.dump(self.cookies, f, indent=2)
 
         # save progress
         progress_path = pathlib.Path('config/progress.json')
@@ -72,14 +72,33 @@ class BeikeMapSpider:
         # close database connection
         self.db_conn.close()
         return False
+    
+    async def init_browser(self, browser: Chrome, headless: bool):
+        browser.options.headless = headless
+        browser.options.add_argument(
+            '--user-agent='
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/138.0.0.0 '
+            'Safari/537.36'
+        )
+        tab = await browser.start()
+        await browser.set_window_maximized()
+        await tab.go_to('https://sh.ke.com/')
+        if self.cookies is not None:
+            await tab.set_cookies([
+                CookieParam(**cookie) for cookie in self.cookies
+            ])
+            await tab.refresh()
+        return tab
 
     async def login(self) -> bool:
         async with Chrome() as browser:
-            await self.init_browser(browser)
-            await self.tab.go_to('https://sh.ke.com/')
-            login_btn = await self.tab.find(class_name="btn-login", timeout=5)
+            tab = await self.init_browser(browser, headless=False)
+            await tab.go_to('https://sh.ke.com/')
+            login_btn = await tab.find(class_name="btn-login", timeout=5)
             await login_btn.click()
-            qr_login_btn = await self.tab.query(
+            qr_login_btn = await tab.query(
                 '//*[@id="loginModel"]/div[2]/div[2]/div[4]/div[1]/ul/li[1]',
                 timeout=5
             )
@@ -88,33 +107,14 @@ class BeikeMapSpider:
             start_time, timeout = time.time(), 30
             while not login_success and time.time() < start_time + timeout:
                 print(f'Checking for login status: {login_success}')
-                login_success = await self.check_is_login()
+                login_info = await tab.find(
+                    class_name="typeShowUser", timeout=5
+                )
+                login_success = (await login_info.text).strip().endswith('退出')
                 if login_success:
-                    self.cookies = await self.tab.get_cookies()
+                    self.cookies = await tab.get_cookies()
                 await asyncio.sleep(2)
         return login_success
-
-    async def check_is_login(self) -> bool:
-        login_info = await self.tab.find(class_name="typeShowUser", timeout=5)
-        return (await login_info.text).strip().endswith('退出')
-
-    async def init_browser(self, browser: Chrome):
-        browser.options.headless = self.headless
-        browser.options.add_argument(
-            '--user-agent='
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
-            'AppleWebKit/537.36 (KHTML, like Gecko) '
-            'Chrome/138.0.0.0 '
-            'Safari/537.36'
-        )
-        self.tab = await browser.start()
-        await browser.set_window_maximized()
-        if self.cookies is not None:
-            await self.tab.go_to('https://sh.ke.com/')
-            await self.tab.set_cookies([
-                CookieParam(**cookie) for cookie in self.cookies
-            ])
-            await self.tab.refresh()
 
     @staticmethod
     def get_bubble_list_url(
@@ -139,11 +139,26 @@ class BeikeMapSpider:
         param_str = '&'.join([f'{k}={v}' for k, v in params.items()])
         return base_url + '?' + param_str
     
+    @staticmethod
+    def get_house_list_url(community_id: int, page: int):
+        params = {
+            'cityId': 310000,
+            'dataSource': 'ESF',
+            'curPage': page,
+            'resblockId': community_id,
+        }
+        base_url = (
+            'https://map.ke.com/proxyApi/i.c-pc-webapi.ke.com/map/houselist'
+        )
+        param_str = '&'.join([f'{k}={v}' for k, v in params.items()])
+        return base_url + '?' + param_str
+    
     def df_to_db(
         self, 
         df: pd.DataFrame, 
         table_name: str, 
-        drop_table: bool
+        drop_table: bool = False,
+        pk_column: str = 'id'
     ):
         if len(df) == 0:
             return
@@ -151,18 +166,19 @@ class BeikeMapSpider:
             self.db_conn.execute(f'drop table if exists {table_name};')
         df['ds'] = self.ds
         df.head(0).to_sql(table_name, self.db_conn, if_exists='append')
-        id_list_str = ', '.join([f"'{i}'" for i in df['id']])
+        pk_list_str = ', '.join([f"'{i}'" for i in df[pk_column]])
         self.db_conn.execute(
             f"delete from {table_name} "
-            f"where id in ({id_list_str})"
+            f"where {pk_column} in ({pk_list_str})"
             f"and ds = '{self.ds}';"
         )
         df.to_sql(table_name, self.db_conn, if_exists='append')
 
-    async def crawl_districts(self, drop_table=False):
+    async def crawl_districts(self, tab: Tab, drop_table=False):
         if self.progress['district_finished']:
             print(f'District list is finished for date {self.ds}')
             return
+        print('Crawling district bubble list')
         url = self.get_bubble_list_url(
             group_type='district',
             min_latitude=self.MIN_LATITUTE,
@@ -170,14 +186,14 @@ class BeikeMapSpider:
             min_longitude=self.MIN_LONGITUDE,
             max_longitude=self.MAX_LONGITUDE,
         )
-        await self.tab.go_to(url)
-        json_element = await self.tab.find(tag_name='pre', timeout=5)
+        await tab.go_to(url)
+        json_element = await tab.find(tag_name='pre', timeout=5)
         data = json.loads(await json_element.text)
         df = pd.DataFrame(data['data']['bubbleList'])
         self.df_to_db(df, 'districts', drop_table)
         self.progress['district_finished'] = True
     
-    async def crawl_bizcircles(self, drop_table=False):
+    async def crawl_bizcircles(self, tab: Tab, drop_table=False):
         if self.progress['bizcircle_latitute'] >= self.MAX_LATITUTE:
             print(f'Bizcircle list is finished for date {self.ds}')
             return
@@ -197,8 +213,8 @@ class BeikeMapSpider:
                     min_longitude=round(longitude, 2),
                     max_longitude=round(longitude + step, 2),
                 )
-                await self.tab.go_to(url)
-                json_element = await self.tab.find(tag_name='pre', timeout=5)
+                await tab.go_to(url)
+                json_element = await tab.find(tag_name='pre', timeout=5)
                 data = json.loads(await json_element.text)
                 if data['data']['totalCount'] > 0:
                     df = pd.DataFrame(data['data']['bubbleList'])
@@ -210,6 +226,7 @@ class BeikeMapSpider:
 
     async def crawl_communities(
         self, 
+        tab: Tab, 
         drop_table: bool = False,
         wait_interval: int = 2
     ):
@@ -232,8 +249,8 @@ class BeikeMapSpider:
                     min_longitude=round(longitude, 2),
                     max_longitude=round(longitude + step, 2),
                 )
-                await self.tab.go_to(url)
-                json_element = await self.tab.find(tag_name='pre', timeout=5)
+                await tab.go_to(url)
+                json_element = await tab.find(tag_name='pre', timeout=5)
                 data = json.loads(await json_element.text)
                 if data['data']['totalCount'] > 0:
                     df = pd.DataFrame(data['data']['bubbleList'])
@@ -242,32 +259,69 @@ class BeikeMapSpider:
                 await asyncio.sleep(wait_interval)
             latitute += step
             self.progress['community_latitute'] = round(latitute, 2)
+            
+    async def crawl_houses(self, tab: Tab, drop_table: bool = False):
+        all_communities = self.db_conn.execute(
+            f"select distinct id from communities where ds = '{self.ds}'"
+        ).fetchall()
+        target_communities = [
+            record[0]
+            for record in all_communities
+            if record[0] >= self.progress['house_community']
+        ]
+        if not target_communities:
+            print(f'House list is finished for date {self.ds}')
+        target_communities.sort()
+        for community_id in target_communities:
+            page = 1
+            while True:
+                print(
+                    f'Crawling house list for community '
+                    f'{community_id} page {page}'
+                )
+                url = self.get_house_list_url(community_id, page)
+                await tab.go_to(url)
+                json_element = await tab.find(tag_name='pre', timeout=5)
+                data = json.loads(await json_element.text)
+                house_list = data['data']['list']
+                for house in house_list:
+                    house['tags'] = '|'.join([
+                        tag['desc'] for tag in house['tags']
+                    ])
+                df = pd.DataFrame(house_list)
+                self.df_to_db(df, 'houses', drop_table, 'actionUrl')
+                if data['data']['hasMore'] == 0:
+                    break
+                page += 1
+                await asyncio.sleep(2)
+            self.progress['house_community'] = community_id
 
-    async def run(self):
+    async def run(self, headless):
         async with Chrome() as browser:
-            await self.init_browser(browser)
-            if not await self.check_is_login():
+            tab = await self.init_browser(browser, headless=headless)
+            login_info = await tab.find(class_name="typeShowUser", timeout=5)
+            is_login = (await login_info.text).strip().endswith('退出')
+            if not is_login:
                 print('Credential expired, please login again...')
-                login_success = await self.login()
-                if not login_success:
-                    print('Login timeout')
-                    return
+                return
+            await self.crawl_districts(tab)
+            await self.crawl_bizcircles(tab)
+            await self.crawl_communities(tab)
+            await self.crawl_houses(tab)
+    
+    async def test(self):
         async with Chrome() as browser:
-            await self.init_browser(browser)
-            await self.crawl_districts()
-            await self.crawl_bizcircles()
-            await self.crawl_communities()
+            tab = await self.init_browser(browser, headless=False)
+            input()
+        
 
 
 async def main():
-    with BeikeMapSpider(headless=False) as spider:
-        await spider.run()
+    with BeikeMapSpider() as spider:
+        # await spider.login()
+        await spider.run(headless=False)
+        # await spider.test()
         
 
 if __name__ == '__main__':
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print('Spider interrupted')
-    except Exception as e:
-        print(f'Other fatal error happened: {e}')
+    asyncio.run(main())
